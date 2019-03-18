8ab5c19ec1aa75e1626cca8e861a89a1f1ed5e94
Stiven <sduque866@gmail.com>
2019-03-17 21:56:28 -0500

    se eliminan todos lo warnings

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,13 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import RefreshIcon from '@material-ui/icons/Refresh';
 import DescriptionIcon from '@material-ui/icons/Description';
 
 const styles = theme => ({
